d7bdeb999d8e75ecdc653d8d132f1e2fefac03a7
Artem Agvanian <artemagvanian@icloud.com>
2023-08-29 13:16:44 -0400

    force cargo to use Cargo.lock when recompiling

--- analyze.sh
+++ analyze.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export RUSTFLAGS="$RUSTFLAGS -Zalways-encode-mir"
-cargo install --path . && cd $1 && cargo clean && cargo pure-func --function $2
+cargo install --locked --path . && cd $1 && cargo clean && cargo pure-func --function $2
